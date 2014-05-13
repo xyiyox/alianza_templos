6992c779822811e5a253c5fa55846f2bd51af86c
Yiyo Castillo <yiyocx@gmail.com>
2014-05-13 01:25:59 -0500

    Se normalizaron un poco los modelos

--- db/admin.py
+++ db/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from models import Proyecto
 
-admin.register(Proyecto)
+admin.site.register(Proyecto)
--- db/models.py
+++ db/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+"""
 class Proyecto(models.Model):
-	"""
+	""" """
 	Representacion de un Proyecto de construccion 
 	de un templo para una iglesia de la IACYMC
-	"""
+	""" """
 	nombre = models.CharField(max_length=40)
 	direccion = models.TextField()
 	# TODO: mejorar geolocalizacion con geodjango
@@ -18,14 +19,14 @@ class Proyecto(models.Model):
 		(2, 'Lideres de la Iglesia'),
 		(3, 'Otro'),
 	)
-	owner_escritura = models.SmallIntegerField()
+	owner_escritura = models.SmallIntegerField('Dueño de la escritura', choices=TIPO_OWNER_ESCRITURA_CHOICES)
 	TIPO_ADQUISICION_CHOICES = (
 		(0, 'Comprado'),
 		(1, 'Donado'),
 	)
 	tipo_adquisicion = models.SmallIntegerField()
 
-	""" Informacion de la comunidad """
+	""" """ Informacion de la comunidad """ """
 	poblacion_comunidad = models.CharField(max_length=40)
 	nombre_comunidad = models.CharField(max_length=50)
 	region = models.CharField(max_length=30) # Tambien puede ser estado, region o distrito
@@ -36,7 +37,7 @@ class Proyecto(models.Model):
 	iglesia_cercana = models.CharField(max_length=30)
 	distancia_iglesia_cercana = models.PositiveSmallIntegerField() # En km
 
-	""" Informacion de la congregacion """
+	""" """ Informacion de la congregacion """ """
 	lengua_primaria = models.CharField(max_length=20)
 	fecha_fundacion = models.DateField()
 	ingresos_ofrendas = models.DecimalField(max_digits=15, decimal_places=3) # ¿Unidad del dinero? Mensual
@@ -45,7 +46,7 @@ class Proyecto(models.Model):
 	cant_miembros_adultos = models.SmallIntegerField()
 	cant_miembtos_ninos = models.SmallIntegerField()
 
-	""" Informacion del pastor """
+	""" """ Informacion del pastor """ """
 	nombre_pastor = models.CharField(max_length=50)
 	entrenamiento_biblico = models.CharField(max_length=50)
 	titulos_obtenidos = models.CharField(max_length=50)
@@ -64,7 +65,7 @@ class Proyecto(models.Model):
 	q2_why_not = models.TextField()
 	q2_how_do = models.TextField()
 
-	""" Informacion de construccion """
+	""" """ Informacion de construccion """ """
 	tiempo_terminar_construccion = models.PositiveSmallIntegerField()
 	dimensiones_terreno = models.CharField(max_length=30)
 	dimensiones_edificio = models.CharField(max_length=30)
@@ -84,6 +85,73 @@ class Proyecto(models.Model):
 	moneda_local = models.CharField(max_length=20)
 	dolar_en_moneda_local = models.DecimalField(max_digits=6, decimal_places=3)
 
+	""" """ Informacion Financiera """ """
+	# Contribuciones estimadas de la congregacion
+	labor_moneda_local = models.DecimalField(max_digits=12, decimal_places=3)
+	labor_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+	materiales_moneda_local = models.DecimalField(max_digits=15, decimal_places=3)
+	materiales_dolares = models.DecimalField(max_digits=15, decimal_places=3)
+	dinero_moneda_local = models.DecimalField(max_digits=15, decimal_places=3)
+	dinero_dolares = models.DecimalField(max_digits=15, decimal_places=3)
+	terreno_moneda_local = models.DecimalField(max_digits=12, decimal_places=3)
+	terreno_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+
+	""" """ Otras fuentes de fondos """ """
+	fuente1 = models.CharField(max_length=30)
+	aporte_fuente1_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
+	aporte_fuente1_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+	fuente2 = models.CharField(max_length=30)
+	aporte_fuente2_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
+	aporte_fuente2_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+	aporte_solicitado_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
+	aporte_solicitado_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+	coste_total_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
+	coste_total_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+"""
+
+class Edificacion(object):
+	"""
+	Representacion de un Proyecto de construccion 
+	de un templo para una iglesia de la IACYMC
+	"""
+	nombre_proyecto = models.CharField(max_length=40)
+	direccion = models.TextField()
+	# TODO: mejorar geolocalizacion con geodjango
+	latitud = models.DecimalField(max_digits=8, decimal_places=4)
+	longitud = models.DecimalField(max_digits=8, decimal_places=4)
+	TIPO_OWNER_ESCRITURA_CHOICES = (
+		(0, 'Partner Organization'),
+		(1, 'Congregacion'),
+		(2, 'Lideres de la Iglesia'),
+		(3, 'Otro'),
+	)
+	owner_escritura = models.SmallIntegerField('Dueño de la escritura', choices=TIPO_OWNER_ESCRITURA_CHOICES)
+	TIPO_ADQUISICION_CHOICES = (
+		(0, 'Comprado'),
+		(1, 'Donado'),
+	)
+	tipo_adquisicion = models.SmallIntegerField()
+
+	tiempo_limite = models.PositiveSmallIntegerField()
+	dimensiones_terreno = models.CharField(max_length=30)
+	dimensiones_edificio = models.CharField(max_length=30)
+	TIPO_CONSTRUCCION_CHOICES = (
+		(0, 'Iglesia'),
+		(1, 'Capilla'),
+		(2, 'Otro'),
+	)
+	tipo_construccion = models.SmallIntegerField()
+	METODO_CONSTRUCCION_CHOICES = (
+		(0, 'Nueva Edificacion'),
+		(1, 'Otro'),
+	)
+	metodo_construccion = models.SmallIntegerField()
+
+	requiere_permiso = models.BooleanField()
+	is_icm_approved = models.BooleanField()
+	moneda_local = models.CharField(max_length=20)
+	dolar_moneda_local = models.DecimalField(max_digits=6, decimal_places=3)
+
 	""" Informacion Financiera """
 	# Contribuciones estimadas de la congregacion
 	labor_moneda_local = models.DecimalField(max_digits=12, decimal_places=3)
@@ -95,14 +163,53 @@ class Proyecto(models.Model):
 	terreno_moneda_local = models.DecimalField(max_digits=12, decimal_places=3)
 	terreno_dolares = models.DecimalField(max_digits=12, decimal_places=3)
 
-	""" Otras fuentes de fondos """
-	fuente1 = models.CharField(max_length=30)
-	aporte_fuente1_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
-	aporte_fuente1_dolares = models.DecimalField(max_digits=12, decimal_places=3)
-	fuente2 = models.CharField(max_length=30)
-	aporte_fuente2_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
-	aporte_fuente2_dolares = models.DecimalField(max_digits=12, decimal_places=3)
-	aporte_solicitado_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
-	aporte_solicitado_dolares = models.DecimalField(max_digits=12, decimal_places=3)
-	coste_total_monedalocal = models.DecimalField(max_digits=12, decimal_places=3)
-	coste_total_dolares = models.DecimalField(max_digits=12, decimal_places=3)
+class Comunidad(models.Model):
+	""" Informacion de la comunidad """
+	poblacion_comunidad = models.CharField(max_length=40)
+	nombre = models.CharField(max_length=50)
+	region = models.CharField(max_length=30) # Tambien puede ser estado, region o distrito
+	pais = models.CharField(max_length=20)
+	
+	ciudad_cercana = models.CharField(max_length=30)
+	distancia_ciudad= models.PositiveSmallIntegerField() # En km
+	iglesia_cercana = models.CharField(max_length=30)
+	distancia_iglesia = models.PositiveSmallIntegerField() # En km
+
+class Congregacion(models.Model):
+	lengua_primaria = models.CharField(max_length=20)
+	fecha_fundacion = models.DateField()
+	asistentes_adultos = models.SmallIntegerField()
+	asistentes_ninos = models.SmallIntegerField()
+	miembros_adultos = models.SmallIntegerField()
+	miembros_ninos = models.SmallIntegerField()
+
+	ingresos_ofrendas = models.DecimalField(max_digits=15, decimal_places=3) # ¿Unidad del dinero? Mensual
+
+	q1_hay_material_biblico = models.BooleanField() # ¿El pastor ha hablado de la disponibilidad de material para estudio biblico?
+	q1_why_not = models.TextField()
+	q2_usa_material = models.BooleanField() # ¿El pastor ha acordado usar este material para crecimiento de la iglesia?
+	q2_why_not = models.TextField()
+	q2_how_do = models.TextField()
+
+class Pastor(models.Model):
+	nombre = models.CharField(max_length=50)
+	entrenamiento_biblico = models.CharField(max_length=50)
+	ESTADO_CIVIL_CHOICES = (
+		(0, 'Soltero'),
+		(1, 'Casado'),
+	)
+	estado_civil = models.SmallIntegerField()
+	numero_hijos = models.PositiveSmallIntegerField()
+	titulos_obtenidos = models.CharField(max_length=50)
+	anios_iglesia = models.PositiveSmallIntegerField()
+	anios_ministerio = models.PositiveSmallIntegerField()
+	# Se debe almacenar una foto del pastor
+	
+class Fuentes_Financieras(models.Model):
+	""" 
+	Tabla que almacenara entradas economicas como mano de obra, materiales
+	"""
+	nombre = models.CharField(max_length=30)
+	descripcion = models.TextField()
+	valor_local = models.DecimalField(max_digits=15, decimal_places=3)
+	valor_dolares = models.DecimalField(max_digits=15, decimal_places=3)
