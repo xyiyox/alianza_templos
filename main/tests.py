75f9e234163294e0c05004664046802c77171bfe
Yiyo Castillo <yiyocx@gmail.com>
2014-06-26 18:05:41 -0500

    Agregados Test para verificar la funcionalidad de las vistas

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from usuarios.models import Usuario
 
 # Create your tests here.
+class SimpleTest(TestCase):
+	
+	def setUp(self):
+		self.usuario = Usuario.objects.create_user(email='prueba', nombre='Prueba', 
+			apellidos='Prueba', password='prueba')
+
+	def test_views(self):
+
+		# Prueba para la vista del home en caso de estar logueado
+		self.client.login(email='prueba', password='prueba')
+		res = self.client.get(reverse('home'))
+		self.assertEqual(res.status_code, 200)
+		# Prueba para la vista del home en caso de no estar logueado
+		self.client.logout()
+		res = self.client.get(reverse('home'))
+		self.assertEqual(res.status_code, 302)
+		
