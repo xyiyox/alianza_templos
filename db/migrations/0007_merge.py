488c28976fc217af1df19cc1abc64dfb872a5f02
Dieguisimo <diegofernando83@gmail.com>
2014-12-22 17:01:39 -0500

    makemigrate --merge para arreglar conflicto de migraciones

---
+++ db/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('db', '0006_auto_20141222_1456'),
+        ('db', '0006_adjuntos_planos_arquitecto'),
+    ]
+
+    operations = [
+    ]
