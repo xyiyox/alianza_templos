75fcbf420f9040ef6ac71b87ed9a3b3c841b6b56
Felipe Valencia <felipe.valencia@laalianzacristiana.co>
2015-05-12 10:30:25 -0500

    Merge de migraciones

---
+++ db/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('db', '0015_remove_edificacion_metodo_construccion'),
+        ('db', '0014_auto_20150512_1003'),
+    ]
+
+    operations = [
+    ]
