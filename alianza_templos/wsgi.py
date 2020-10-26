9cfa5607ea5f27b9f16a1c2d5502ab6fe34732a6
Diego Bolivar <diegofernando83@gmail.com>
2020-10-26 00:50:03 -0500

    nombre de app en wsgi.py corregido

--- alianza_templos/wsgi.py
+++ alianza_templos/wsgi.py
@@ -1,9 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alliance.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alianza_templos.settings")
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-#import newrelic.agent
-#newrelic.agent.initialize(os.path.join(BASE_DIR, 'alliance/newrelic.ini'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
